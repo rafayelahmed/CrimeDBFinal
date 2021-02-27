149435e2e9db6f0f894a68bead7c3c9d33084295
rafa <rafayelhtc@gmail.com>
2021-02-27 13:51:35 +1100

    CD-5 commit jira

--- Tables/dbo.DimCountry.sql
+++ Tables/dbo.DimCountry.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[DimCountry]
 (
-[cid] [int] NULL,
-[cname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[countryid] [int] NULL,
+[countryname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
