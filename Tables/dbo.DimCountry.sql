fed732a0248192ec83cd7757011c5f2550aa0506
rafa <rafayelhtc@gmail.com>
2021-02-27 13:56:44 +1100

    CD-5  Initial commit

--- Tables/dbo.DimCountry.sql
+++ Tables/dbo.DimCountry.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[DimCountry]
 (
 [countryid] [int] NULL,
-[countryname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[countryname] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ISNew] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
